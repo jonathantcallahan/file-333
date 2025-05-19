38bb097c977354adf20a869fcee7d026a163a05c
Jonathan Callahan <jtc7537@gmail.com>
2025-05-19 12:03:08 -0500

    controller

--- file 333/Assets/Scenes/Scripts/MoveToBallAgent.cs
+++ file 333/Assets/Scenes/Scripts/MoveToBallAgent.cs
@@ -3,12 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class MoveToBallAgent: Agent
 {
+    [SerializeField] private Transform targetTransform;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.position = Vector3.zero;
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        Debug.Log(actions.DiscreteActions[0]);
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 2f;
+        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed; 
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if ( other.gameObject.tag == "Goal")
+        {
+            SetReward(+1f);
+            EndEpisode();
+        }
+        if (other.gameObject.tag == "Wall")
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
     }
 
 }
